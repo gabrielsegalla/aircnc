3841f53ae0c39a231f229acffaabb076c9c02f53
Gabriel Rodrigues Segalla <Gabriel.Segalla@agrosatelite.local>
2019-10-04 21:28:07 -0300

    Finalizando a Omnistack

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io')
+const http = require('http')
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-kstui.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser:true,
@@ -12,9 +17,23 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-kstui.mongodb.net/s
   bufferCommands:false,
 })
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query
+  connectedUsers[user_id] = socket.id;
+});
+
+
+app.use((req,res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
